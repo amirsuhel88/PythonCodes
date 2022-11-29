069368cdde7ea4f453cbabe3dff5f5e175ad2e63
suhel <amirsuhel88@gmail.com>
2022-11-29 22:12:23 +0530

    My first try in python

---
+++ quotes.py
@@ -0,0 +1,15 @@
+file = "Suhel's file"
+print(file)
+
+file1 = 'Suhel said, "Do not go there"'
+print(file1)
+
+file2 = '''Suhel said, "Don't go there"'''
+print(file2)
+print(file2[3]) #prints [3] letter
+print(file2[1:5])
+print(file2[2:])
+print(file2[:3])
+file3 = 'This is me suhel'
+another = file3[:]
+print(another)
---
+++ weight.py
@@ -0,0 +1,4 @@
+weight_pounds = int(input('Enter your weight in pounds: '))
+weight_kg = weight_pounds * 0.45
+print('Your weight in KG is: ')
+print(weight_kg)
